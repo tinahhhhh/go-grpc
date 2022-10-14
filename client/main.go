84aa68539b9ca246813f6a2522ce872a5844c637
Tina Huang <tina84318@yahoo.com.tw>
2022-10-14 13:09:34 +0200

    continuous input

--- client/main.go
+++ client/main.go
@@ -1,49 +1,59 @@
 package main
 
 import (
-	"context"
-	"log"
-	"flag"
-	"time"
-	"bufio"
-	"os"
-	"fmt"
+    "context"
+    "log"
+    "flag"
+    "time"
+    "bufio"
+    "os"
+    "fmt"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	pb "github.com/tinahhhhh/go-grpc/spam"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials/insecure"
+    pb "github.com/tinahhhhh/go-grpc/spam"
 )
 
 
 var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+    addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
 func main() {
-	flag.Parse()
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewAssessmentClient(conn)
+    flag.Parse()
+    // Set up a connection to the server.
+    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        log.Fatalf("did not connect: %v", err)
+    }
+    defer conn.Close()
+    c := pb.NewAssessmentClient(conn)
 
     // client input
     reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Input: ")
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
+    fmt.Print("Input: ")
+    text, err := reader.ReadString('\n')
+    if err != nil {
+        panic(err)
+    }
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-	defer cancel()
+    for text != "q\n" {
+        // Contact the server and print out its response.
+        ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+        defer cancel()
 
-	r, err := c.CheckSpam(ctx, &pb.AssessmentRequest{Entity: text})
-	if err != nil {
-		log.Fatalf("could not assess: %v", err)
-	}
-	log.Printf(r.GetMessage())
+        r, err := c.CheckSpam(ctx, &pb.AssessmentRequest{Entity: text})
+        if err != nil {
+            log.Fatalf("could not assess: %v", err)
+        }
+        log.Printf(r.GetMessage())
+
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print("Input: ")
+        text, err = reader.ReadString('\n')
+        if err != nil {
+            panic(err)
+        }
+    }
+    
 }
